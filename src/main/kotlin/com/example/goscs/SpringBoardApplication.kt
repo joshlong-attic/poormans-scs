e061fdf2d9ca6f2fd167144484af1481657bc65a
Josh Long <josh@joshlong.com>
2017-10-25 18:41:56 -0700

    initial commit of SCS

---
+++ src/main/kotlin/com/example/goscs/SpringBoardApplication.kt
@@ -0,0 +1,42 @@
+package com.example.goscs
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.Resource
+import reactor.core.publisher.Flux
+import reactor.core.scheduler.Schedulers
+import java.io.File
+import java.time.Duration
+
+@SpringBootApplication
+class SpringBoardApplication {
+
+    @Bean
+    fun run(@Value("file:///\${SCS_ROOT:\${user.home}/Desktop/spring-cloud-services}") dir: Resource)
+            = ApplicationRunner {
+
+        dir
+                .file
+                .run {
+                    Flux.just("config-service", "eureka-service", "zipkin-service").map { File(this, it) }
+                }
+                .subscribeOn(Schedulers.elastic()) // we want all three running at once, so concurrent scheduling is important
+                .map {
+                    ProcessBuilder("/bin/bash")
+                            .directory(it)
+                            .command("mvn", "spring-boot:run")
+                }
+                .delayElements(Duration.ofSeconds(10)) // launch each one 10s after the first one
+                .subscribe {
+                    println("about to start ${it.command()} in ${it.directory()}")
+                    it.start()
+                }
+    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringBoardApplication::class.java, *args)
+}
